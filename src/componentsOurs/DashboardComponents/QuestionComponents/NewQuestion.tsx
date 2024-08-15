68a83eeec6bebf785f421426d6bebbdfc2eb5e13
Kastriot <kaloti.avdiaj@gmail.com>
2024-08-15 23:55:30 +0200

    Created the new question form, where the admin can choose how many wrong answers there will be and how difficult the answer is going to be.

--- src/componentsOurs/DashboardComponents/QuestionComponents/NewQuestion.tsx
+++ src/componentsOurs/DashboardComponents/QuestionComponents/NewQuestion.tsx
@@ -1,49 +1,132 @@
-import * as React from "react"
-
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
+import { useState } from "react";
 
-export function NewQuestion() {
+type NewQuestionProps = {
+  setIsOpen: (value: boolean) => void;
+  isOpen: boolean;
+};
+
+export const NewQuestion: React.FC<NewQuestionProps> = ({
+  isOpen,
+  setIsOpen,
+}) => {
+  const [numWrongAnswers, setNumWrongAnswers] = useState<number>(3);
+
+  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNumWrongAnswers(Number(event.target.value));
+  };
+
   return (
-    <Card className="w-[350px]">
+    <Card
+      className={`w-[600px] m-6 border-none bg-[var(--dashboard)] rounded-sm ${
+        isOpen ? "visible" : "hidden"
+      }`}
+    >
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardTitle>Create a new question</CardTitle>
       </CardHeader>
       <CardContent>
         <form>
-          <div className="grid w-full items-center gap-4">
+          <div className="grid w-full items-center gap-8">
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
+              <Label htmlFor="name">Question</Label>
+              <Input
+                id="name"
+                placeholder="Question"
+                className="placeholder-gray-500 !placeholder-gray-400 border-gray-400"
+              />
             </div>
+            <div className="flex flex-col space-y-1.5 ml-0.5">
+              <Label>Select the amount of wrong answers?</Label>
+              <div className="flex space-x-4">
+                {[1, 2, 3].map((value) => (
+                  <label key={value} className="flex items-center space-x-2">
+                    <Input
+                      type="radio"
+                      name="numWrongAnswers"
+                      value={value}
+                      checked={numWrongAnswers === value}
+                      onChange={handleRadioChange}
+                      className="border-gray-400 ml-2 cursor-pointer"
+                    />
+                    <span>{value}</span>
+                  </label>
+                ))}
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="correct-answer">Correct Answer *</Label>
+                <Input
+                  id="correct-answer"
+                  placeholder="Correct Answer"
+                  className="placeholder-gray-500 !placeholder-gray-400 border-gray-400"
+                />
+              </div>
+              {[...Array(numWrongAnswers)].map((_, index) => (
+                <div key={index} className="flex flex-col space-y-1.5">
+                  <Label htmlFor={`wrong-answer-${index + 1}`}>
+                    Wrong Answer {index + 1}
+                  </Label>
+                  <Input
+                    id={`wrong-answer-${index + 1}`}
+                    placeholder={`Wrong Answer ${index + 1}`}
+                    className="placeholder-gray-500 !placeholder-gray-400 border-gray-400"
+                  />
+                </div>
+              ))}
+            </div>
+
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Framework</Label>
+              <Label htmlFor="framework">Difficulty</Label>
               <Select>
-                <SelectTrigger id="framework">
+                <SelectTrigger id="framework" className="border-gray-400">
                   <SelectValue placeholder="Select" />
                 </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="next">Next.js</SelectItem>
-                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                  <SelectItem value="astro">Astro</SelectItem>
-                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                <SelectContent
+                  position="popper"
+                  className="cursor-pointer bg-[var(--dashboard)]"
+                >
+                  <SelectItem
+                    className="cursor-pointer hover:!bg-[var(--dashboard-lighter)]"
+                    value="next"
+                  >
+                    Extra Hard
+                  </SelectItem>
+                  <SelectItem
+                    className="cursor-pointer hover:!bg-[var(--dashboard-lighter)]"
+                    value="sveltekit"
+                  >
+                    Hard
+                  </SelectItem>
+                  <SelectItem
+                    className="cursor-pointer hover:!bg-[var(--dashboard-lighter)]"
+                    value="astro"
+                  >
+                    Medium
+                  </SelectItem>
+                  <SelectItem
+                    className="cursor-pointer hover:!bg-[var(--dashboard-lighter)]"
+                    value="nuxt"
+                  >
+                    Easy
+                  </SelectItem>
                 </SelectContent>
               </Select>
             </div>
@@ -51,9 +134,17 @@ export function NewQuestion() {
         </form>
       </CardContent>
       <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
+        <Button variant="outline" onClick={() => setIsOpen(false)}>
+          Cancel
+        </Button>
+        <Button
+          variant={"addSave"}
+          className="rounded-sm"
+          onClick={() => setIsOpen(false)}
+        >
+          Create
+        </Button>
       </CardFooter>
     </Card>
-  )
-}
+  );
+};
--- src/pages/DashboardPages/Questions.tsx
+++ src/pages/DashboardPages/Questions.tsx
@@ -1,12 +1,31 @@
-
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 import { IoAdd } from "react-icons/io5";
+import { NewQuestion } from "@/componentsOurs/DashboardComponents/QuestionComponents/NewQuestion";
+
 export const Questions = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
   return (
     <div>
-      <Button variant="addSave">
-        <IoAdd className="text-xl"/> New Question
-      </Button>
+      {isOpen ? (
+        <NewQuestion isOpen={isOpen} setIsOpen={setIsOpen} />
+      ) : (
+        <div>
+          <Button
+            variant="addSave"
+            onClick={handleToggle}
+            aria-expanded={isOpen}
+            aria-controls="new-question-form"
+          >
+            <IoAdd className="text-xl" /> New Question
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
