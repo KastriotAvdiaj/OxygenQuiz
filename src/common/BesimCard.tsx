dec4d6238d7dc282d5ea7f5ece7843ce0ed1285b
Kastriot <kaloti.avdiaj@gmail.com>
2024-09-15 04:51:38 +0200

    Re styled the landing page with a new 3d looking card.

---
+++ src/common/BesimCard.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import BesimDina from "../assets/BesimDina.png";
+import { cn } from "@/utils/cn";
+
+interface BesimCardProps {
+  className?: string;
+}
+
+export const BesimCard: React.FC<BesimCardProps> = ({ className }) => {
+  return (
+    <div
+      className={cn(
+        "relative w-[500px] h-[600px] flex items-center justify-center z-10 border-4 border-transparent bg-gradient-to-r from-[var(--text-hover-opposite)] p-[2px] rounded-xl transition-transform transform hover:rotate-3 hover:scale-105 hover:shadow-2xl",
+        className
+      )}
+    >
+      <div className="w-full h-full bg-[var(--text-hover)] rounded-xl shadow-lg transition-none">
+        <img
+          src={BesimDina}
+          alt="Besim Dina"
+          className="w-full h-full object-cover absolute right-[-158px] translate-x-[30px] z-12 pointer-events-none"
+        />
+      </div>
+    </div>
+  );
+};
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,36 @@
-// import { Card } from "@/common/Card";
-// import { Button } from "@/components/ui/button";
+import { BesimCard } from "@/common/BesimCard";
 
 export const Home = () => {
   return (
-    <div
-      className="flex justify-center items-center h-screen gap-6 bg-background"
-    >
-      
+    <div className="relative grid grid-cols-1 lg:grid-cols-2 justify-items-center items-center h-screen w-screen bg-background px-[10rem] gap-2">
+      <div className="relative z-10 space-y-6 ">
+        <h1 className="text-5xl md:text-6xl lg:text-7xl  font-bold text-[var(--text)]">
+          Welcome to <br />{" "}
+          <span className="bg-gradient-to-r from-[var(--text-hover)] via- to-purple-500  bg-clip-text text-transparent">
+            Oxygen Quiz!
+          </span>
+        </h1>
+        <p className="text-xl md:text-2xl lg:text-3xl text-[var(--text)]">
+          Test your knowledge and{" "}
+          <span className="text-[var(--text-hover)] font-semibold">
+            {" "}
+            improve
+          </span>{" "}
+          your skills!
+        </p>
+        <button className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-3 px-6 rounded-lg">
+          Get Started
+        </button>
+      </div>
+      <BesimCard />
     </div>
   );
 };
 
-
-{/* <Card>
+// import { Card } from "@/common/Card";
+// import { Button } from "@/components/ui/button";
+{
+  /* <Card>
         <h1 className="text-4xl font-bold tracking-wide text-center">
           Welcome to the Quiz App!
         </h1>
@@ -33,4 +51,5 @@ export const Home = () => {
             ></path>
           </svg>
         </Button>
-      </Card> */}
+      </Card> */
+}
