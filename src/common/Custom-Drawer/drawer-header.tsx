761c69cb47c63e2128861a5b7f46de6ba484b140
Kastriot <kaloti.avdiaj@gmail.com>
2024-09-27 01:19:07 +0200

    Imporetd the useUser function in the drawer header so that we can display the username.

--- src/common/Custom-Drawer/drawer-header.tsx
+++ src/common/Custom-Drawer/drawer-header.tsx
@@ -1,15 +1,20 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Divider } from "../Divider";
+import { useUser } from "@/lib/Auth";
 
-export const DrawerHeaderContent = () => (
-  <>
-    <div className="flex justify-start gap-4 items-center font-bold mb-6 ">
-      <Avatar className="cursor-pointer ">
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-      Username
-    </div>
-    <Divider orientation="horizontal" thickness="1px" length="100%" />
-  </>
-);
+export const DrawerHeaderContent = () => {
+  const { data: user } = useUser();
+
+  return (
+    <>
+      <div className="flex justify-start gap-4 items-center font-bold my-6 ">
+        <Avatar className="cursor-pointer ">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        {user ? user.username : "User"}
+      </div>
+      <Divider orientation="horizontal" thickness="1px" length="100%" />
+    </>
+  );
+};
