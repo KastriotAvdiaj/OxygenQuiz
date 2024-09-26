9a795e4a4c32335e5956b1b538acb7d2f574a745
Kastriot <kaloti.avdiaj@gmail.com>
2024-09-27 00:58:38 +0200

    Changed the questions page to include the new form drawer instead of the old one.

--- src/pages/Dashboard/Pages/Question/Questions.tsx
+++ src/pages/Dashboard/Pages/Question/Questions.tsx
@@ -1,31 +1,42 @@
 import { Button } from "@/components/ui/button";
-import { IoAdd } from "react-icons/io5";
-import { FormDrawer } from "@/common/Form-Drawer";
+import { FormDrawer } from "@/components/ui/form";
 import { NewQuestion } from "./Components/create-question";
-import { useDisclosure } from "@/hooks/use-disclosure";
+import { Plus } from "lucide-react";
 import { DataTable } from "@/components/ui";
 import { columns } from "./Components/columns";
 
 export const Questions = () => {
-  const { isOpen, open, close, toggle } = useDisclosure();
   return (
     <div>
-      <Button
-        variant="addSave"
-        onClick={open}
-        aria-expanded={isOpen}
-        className="w-fit text-white"
-        aria-controls="new-question-form"
-      >
-        <IoAdd className="text-xl" /> New Question
-      </Button>
       <FormDrawer
-        className="w-fit"
-        form={<NewQuestion />}
-        isOpen={isOpen}
-        toggle={toggle}
-        onClose={close}
-      />
+      isDone={false}
+      triggerButton={
+        <Button
+          variant="addSave"
+          className="bg-text-hover rounded-sm"
+          size="sm"
+          icon={<Plus className="size-4" />}
+        >
+          Create Question
+        </Button>
+      }
+      title="Create User"
+      submitButton={
+        <Button
+          form="create-user"
+          variant="addSave"
+          className="rounded-sm"
+          type="submit"
+          size="default"
+          isPending={true}
+          disabled={false}
+        >
+          Submit
+        </Button>
+      }
+    >
+      <NewQuestion />
+    </FormDrawer>
       <DataTable columns={columns} data={[]} />
     </div>
   );
