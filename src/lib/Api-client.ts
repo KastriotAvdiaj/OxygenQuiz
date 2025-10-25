813fa6462d6d8f19faf94693265cba3badf64c7b
Kastriot <kaloti.avdiaj@gmail.com>
2025-10-26 00:54:41 +0200

    Updated the base url since the backend container had to re-run (and changed its ip)

--- src/lib/Api-client.ts
+++ src/lib/Api-client.ts
@@ -52,7 +52,7 @@ llmApi.interceptors.response.use(
 );
 
 export const api = Axios.create({
-  baseURL: `http://3.70.217.174:8080/api`,
+  baseURL: `http://3.76.222.101:8080/api`,
   // baseURL: `https://localhost:7153/api`,
 });
 
